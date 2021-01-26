6bdedde08717759e1d5c03248303521fec2ce5a3
tcbutler320 <tcbutler320@gmail.com>
2021-01-26 17:54:00 -0500

    seperate functions into packages

---
+++ lib/utils/help.py
@@ -0,0 +1,16 @@
+import sys
+import lib.core.constants as c 
+
+# Print usage options to sysout 
+def usage():
+    sys.stdout.write(c.Red)
+    print(c.synerror)
+    print(c.example)
+    sys.exit(0)
+
+# Print help to sysout 
+def help():
+    sys.stdout.write(c.Red)
+    print(c.synerror)
+    print(c.example)
+    sys.exit(0)
